a9698d97e374edcbb33c6a950621b1027a7710f8
hicka04 <calpissoda8221@gmail.com>
2018-12-17 17:39:32 +0900

    プロジェクト追加

---
+++ repository-pattern-sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  repository-pattern-sample
+//
+//  Created by hicka04 on 2018/12/17.
+//  Copyright © 2018 hicka04. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
